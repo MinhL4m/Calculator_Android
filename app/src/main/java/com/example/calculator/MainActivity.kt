e6a0cc9ffa0493650002ad268be94a122dadfbdf
Minh Lam <ltm1701@gmail.com>
2020-04-15 09:34:01 -0600

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,183 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var resultScreen = "0"
+    private var firstNum = "0"
+    private lateinit var type: String
+
+    private lateinit var resultTextView: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+        resultScreen = savedInstanceState?.getString("result","0")?:"0"
+        type = savedInstanceState?.getString("type","none")?:"none"
+        firstNum = savedInstanceState?.getString("firstNum","0")?:"0"
+
+        resultTextView.text = resultScreen
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putString("result",resultScreen)
+        savedInstanceState.putString("type",type)
+        savedInstanceState.putString("firstNum", firstNum)
+    }
+
+    fun zeroOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "0"
+            resultTextView.text = resultScreen
+        }
+    }
+
+    fun oneOnClick(view: View){
+        if(resultScreen != "0"){
+            resultScreen += "1"
+        }else{
+            resultScreen = "1"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun twoOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "2"
+        }else{
+            resultScreen = "2"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun threeOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "3"
+        }else{
+            resultScreen = "3"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun fourOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "4"
+        }else{
+            resultScreen = "4"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun fiveOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "5"
+        }else{
+            resultScreen = "5"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun sixOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "6"
+        }else{
+            resultScreen = "6"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun sevenOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "7"
+        }else{
+            resultScreen = "7"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun eightOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "8"
+        }else{
+            resultScreen = "8"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun nineOnClick(view:View){
+        if(resultScreen != "0"){
+            resultScreen += "9"
+        }else{
+            resultScreen = "9"
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun timeOnClick(view:View){
+        type = "time"
+        firstNum = resultScreen
+        resultScreen = "0"
+        resultTextView.text = resultScreen
+    }
+
+    fun plusOnClick(view:View){
+        type = "plus"
+        firstNum = resultScreen
+        resultScreen = "0"
+        resultTextView.text = resultScreen
+    }
+
+    fun minusOnClick(view:View) {
+        type = "minus"
+        firstNum = resultScreen
+        resultScreen = "0"
+        resultTextView.text = resultScreen
+    }
+
+    fun equalOnClick(view: View){
+        var resultScreenInt = Integer.parseInt(resultScreen)
+        var firstNumInt = Integer.parseInt(firstNum)
+        firstNum= "0"
+        when(type){
+            "time" -> {
+                resultScreenInt *= firstNumInt
+                resultScreen = resultScreenInt.toString()
+            }
+            "plus" -> {
+                resultScreenInt += firstNumInt
+                resultScreen = resultScreenInt.toString()
+            }
+            "minus"->{
+                resultScreenInt = firstNumInt - resultScreenInt
+                resultScreen = resultScreenInt.toString()
+            }
+        }
+        resultTextView.text = resultScreen
+    }
+
+    fun removeOnClick(view:View){
+        resultScreen = "0"
+        resultTextView.text = resultScreen
+    }
+
+    fun backOnClick(view:View){
+        resultScreen = if(resultScreen.length != 1){
+            resultScreen.slice(0..resultScreen.length - 2)
+        }else{
+            "0"
+        }
+
+        resultTextView.text = resultScreen
+    }
+}
